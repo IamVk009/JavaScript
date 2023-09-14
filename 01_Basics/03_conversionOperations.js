0cd52b89f66d2d374c8eb0a82a8773efba6936ce
IamVk009 <68426241+IamVk009@users.noreply.github.com>
2023-09-14 10:13:59 +0000

    JS TypeConversion

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = null
+console.log(typeof score)
+let toNumber = Number(score)
+console.log(typeof toNumber)
+console.log(toNumber);
+
+// Conversions to Number
+// 333 => 333
+// 333aaa => NaN (Not a Number)
+// true => 1
+// false => 0
+// undefined => NaN
+// null => 0
+
+
+// Convert to Boolean
+let isLoggedIn = "Virat";
+console.log(Boolean(isLoggedIn));
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Virat" => true
+
+// Convert to String
+let number = 1290
+let str = String(number)
+console.log(String(number));
+console.log(typeof str);
